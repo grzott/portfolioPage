efe3032fa00bc259f6b6c8235bdda76af4847f7f
Grzegorz Otto <grzott@gmail.com>
2021-04-02 15:18:55 +0200

    theming logic relocated from gatsby-browser.js to Layout.jsx

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,36 +1,11 @@
 import React from 'react'
-import { ThemeProvider } from 'styled-components';
-import Layout from './src/components/_shared/layout/Layout'
-import { lightTheme, darkTheme, pl, eng } from './src/theme/Themes'
-
-import { Provider, useSelector } from 'react-redux'
+import { Provider } from 'react-redux'
 import store from './src/redux/store'
+import Layout from './src/components/_shared/layout/Layout'
 
-const themePl = (currentTheme) => ({
-    ...currentTheme,
-    ...pl,
-});
-
-const themeEng = (currentTheme) => ({
-    ...currentTheme,
-    ...eng,
-});
-
-const App = ({ element }) => {
-    const toggleTheme = useSelector(state => state.toggleTheme)
-    const toggleLang = useSelector(state => state.toggleLanguage)
-
-    return (
-        <ThemeProvider theme={toggleLang ? themeEng : themePl}>
-            <ThemeProvider theme={toggleTheme ? darkTheme : lightTheme}>
-                <Layout>{element}</Layout>
-            </ThemeProvider>
-        </ThemeProvider>
-    )
-}
 
 export const wrapRootElement = ({ element }) => (
     <Provider store={store}>
-        <App element={element} />
+        <Layout>{element}</Layout>
     </Provider>
 )
