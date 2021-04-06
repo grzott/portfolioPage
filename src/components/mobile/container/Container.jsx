84fe6b566c6c8f2ddd6536a224625c9e9748224e
Grzegorz Otto <grzott@gmail.com>
2021-04-06 09:24:04 +0200

    build fix

--- src/components/mobile/container/Container.jsx
+++ src/components/mobile/container/Container.jsx
@@ -4,7 +4,7 @@ import styled, { withTheme } from "styled-components"
 const MContainer = styled.div`
   max-width: 92%;
   margin: 0 20px;
-  padding: 60px 0 40px 0;
+  padding: 80px 0 40px 0;
 `
 
 const MobileContainer = ({ children, theme }) => {
